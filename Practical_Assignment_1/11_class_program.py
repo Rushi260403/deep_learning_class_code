2a895457de555576983f1de04d6ff87059deeca6
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-05 10:34:14 +0530

    class program

---
+++ Practical_Assignment_1/11_class_program.py
@@ -0,0 +1,13 @@
+import numpy as np
+tensor_1d = np.array([1.3,1,4.0,23.99])
+print(tensor_1d)
+
+print(tensor_1d[0])
+print(tensor_1d[2])
+
+#creating 2d tensor
+tensor_2d = np.array([(1,2,3,4),(4,5,6,7),(8,9,10,11),(12,13,14,15)])
+print(tensor_2d)
+
+print(tensor_2d[2][2])
+print(tensor_2d[3][2])
