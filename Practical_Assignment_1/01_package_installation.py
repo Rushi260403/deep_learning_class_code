e5ff2b3f880c0f4707183fe867a92fe433b4f1c6
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-09 11:55:47 +0530

    initial commit

--- Practical_Assignment_1/01_package_installation.py
+++ Practical_Assignment_1/01_package_installation.py
@@ -1,8 +1,16 @@
 import numpy
-import pandas
 import matplotlib
+import pandas as pd
 import sklearn
-import tensorflow
-import keras
-
+import tensorflow as tf
+print("Pandas version:", pd.__version__)
+print("Scikit-learn imported successfully!")
+print("TensorFlow version:", tf.__version__)
 print("All libraries installed successfully!")
+
+# This will make numpy and matplotlib turn white
+data = numpy.array([1, 2, 3])
+matplotlib.pyplot.plot(data)
+
+# This will make sklearn turn white
+print("Scikit-learn version:", sklearn.__version__)
